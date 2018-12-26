6cdae4a97a315599fa280790846bcb105eb53e5e
Shreif Abdallah <shreifelsaid@gmail.com>
2018-12-25 23:33:28 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 
 int main(int argc, char** argv) {
 	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(52, 41, white);
+	line(13, 20, 80, 40, image, white);
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	return 0;
