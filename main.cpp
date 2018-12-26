37bc13a32313b0a67815a17d4fac813f107a36bd
Shreif Abdallah <shreifelsaid@gmail.com>
2018-12-25 23:30:23 -0500

    Line drawing function

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+	for (float t = 0.; t < 1.; t += .01) {
+		int x = x0 * (1. - t) + x1 * t;
+		int y = y0 * (1. - t) + y1 * t;
+		image.set(x, y, color);
+	}
+}
+
 int main(int argc, char** argv) {
 	TGAImage image(100, 100, TGAImage::RGB);
 	image.set(52, 41, white);
