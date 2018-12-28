939de95a94e2ed21d17ffeea4e0f4df08acb9b74
Shreif Abdallah <shreifelsaid@gmail.com>
2018-12-27 20:51:42 -0500

    update line function, no costly multiplication, just addition/subtraction/ comparison.
    reducing run time to O(1) on each iteration

--- main.cpp
+++ main.cpp
@@ -3,17 +3,59 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
+
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-	for (float t = 0.; t < 1.; t += .01) {
-		int x = x0 * (1. - t) + x1 * t;
-		int y = y0 * (1. - t) + y1 * t;
-		image.set(x, y, color);
+	bool steep = false;
+	float slope = (float)(y1 - y0) / (float)(x1 - x0);
+
+	if (std::abs(x0 - x1) < std::abs(y0 - y1)) { // if rise is bigger than run (slope > 1 or slope < -1), we swap x and y to avoid holes. 
+		std::swap(x0, y0);
+		std::swap(x1, y1);
+		steep = true;
+	}
+
+	//our loop counts pixels in the x direction, therefore we need to "Force" x0 to be always smaller than x1;
+	if (x0 > x1) { 
+		std::swap(x0, x1);
+		std::swap(y0, y1);
+	}
+	int y = y0;
+	float threshold = 0.5;
+	int increment;
+	if (slope > 0) {
+		increment = 1;
+	}
+	else {
+		increment = -1;
+	}
+	float dy = abs(slope);
+	float test = 0;
+
+	//loop that uses number of pixels in the x direction
+	for (int x = x0; x <= x1; x++) {
+
+		if (steep) {
+			image.set(y, x, color); // if the line is steep, we reverse the x and y, here we set them straight by reversing the y and x again.
+		}
+		else {
+			image.set(x, y, color);
+		}
+
+		test= test + dy;
+		if (test > threshold) {
+			y = y + increment; //could be negative if line is sloping downwards
+			//incrementing the threshold variable
+			threshold++;
+		}
 	}
 }
+
+
 
 int main(int argc, char** argv) {
 	TGAImage image(100, 100, TGAImage::RGB);
 	line(13, 20, 80, 40, image, white);
+
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	return 0;
