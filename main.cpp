f98991dbd2b08a390fb00750d03edcff05a3a037
Shreif Abdallah <shreifelsaid@gmail.com>
2018-12-28 22:52:26 -0500

    triangles with fill

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
-#include <vector>
+﻿#include <vector>
 #include <cmath>
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
+#include <algorithm> 
+#include <iostream> 
+
+
 
 Model *model = NULL;
 const int width = 800;
@@ -13,7 +17,11 @@ const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
 
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
+	int x0 = p0.x;
+	int y0 = p0.y;
+	int x1 = p1.x;
+	int y1 = p1.y;
 	bool steep = false;
 	float slope = (float)(y1 - y0) / (float)(x1 - x0);
 
@@ -60,10 +68,40 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 }
 
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
-	line(t0.x, t0.y, t1.x, t1.y, image, color);
-	line(t1.x , t1.y , t2.x , t2.y, image, color);
-	line(t2.x, t2.y, t0.x, t0.y, image, color);
+	// sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
+	if (t0.y > t1.y) std::swap(t0, t1);
+	if (t0.y > t2.y) std::swap(t0, t2);
+	if (t1.y > t2.y) std::swap(t1, t2);
+
+	int total_height = t2.y - t0.y;
+
+	for (int i = t0.y; i <= t1.y; i++) {
+		int segment_height = t1.y - t0.y + 1;
+
+		float x1 = t0.x + (((i - t0.y)*(t1.x-t0.x))/(segment_height));
+		float x2 = t0.x + (((i - t0.y)*(t2.x - t0.x)) / (total_height));
+		if (x1 > x2) std::swap(x1, x2);
+		for (int j = x1; j <= x2; j++) {
+			image.set(j, i, white);
+		}
+		image.set(x1, i, red);
+	    image.set(x2, i, green);
+	}
+
+	for (int i = t1.y; i <= t2.y; i++) {
+		int segment_height = t2.y - t1.y + 1;
+
+		float x1 = t1.x + (((i - t1.y)*(t2.x - t1.x)) / (segment_height));
+		float x2 = t0.x + (((i - t0.y)*(t2.x - t0.x)) / (total_height));
+		if (x1 > x2) std::swap(x1, x2);
+		for (int j = x1; j <= x2; j++) {
+			image.set(j, i, white);
+		}
+		image.set(x1, i, red);
+		image.set(x2, i, green);
+	}
 }
+
 
 
 int main(int argc, char** argv) {
@@ -78,6 +116,7 @@ int main(int argc, char** argv) {
 	triangle(t0[0], t0[1], t0[2], image, red);
 	triangle(t1[0], t1[1], t1[2], image, white);
 	triangle(t2[0], t2[1], t2[2], image, green);
+	
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
